cb7ab27452203751a6325c466d49759bc343bc90
art <art.huang@ehsn.com.tw>
2022-04-13 12:21:37 +0800

    init

---
+++ clientApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using clientApp.Models;
+
+namespace clientApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly HttpClient _httpClient;
+
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var proxyResult = await GetWeatherForecast("https://localhost:7036/weatherforecastproxy");
+        return View("Index", proxyResult);
+    }
+
+    private async Task<string> GetWeatherForecast(string proxyUrl)
+    {
+        return await _httpClient.GetStringAsync(proxyUrl);
+    }
+}
---
+++ frontendApp/Controllers/WeatherForecastProxyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace frontendApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastProxyController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastProxyController> _logger;
+    private readonly HttpClient _httpClient;
+
+    public WeatherForecastProxyController(ILogger<WeatherForecastProxyController> logger,IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClient = httpClientFactory.CreateClient();
+    }
+    [HttpGet]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        var jsonStream = await _httpClient.GetStreamAsync("https://localhost:7250/WeatherForecast");
+        var weatherForecast = await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>(jsonStream);
+        return weatherForecast;
+    }
+}
